b55fc030a08a52e4137d2c117482195f79ba832e
trgomes <trtg10@gmail.com>
2019-02-24 07:55:11 -0300

    multi-bd strategy

---
+++ 07-multi-db/src/db/strategies/mongoDbStrategy.js
@@ -0,0 +1,12 @@
+const IDb = require('./base/interfaceDb');
+
+class MongoDBStrategy extends IDb {
+    constructor() {
+        super();
+    }
+    create(item) {
+        return 'MongoDB';
+    }
+}
+
+module.exports = MongoDBStrategy;
---
+++ 07-multi-db/src/db/strategies/postgresStrategy.js
@@ -0,0 +1,12 @@
+const IDb = require('./base/interfaceDb');
+
+class PostgresStrategy extends IDb {
+    constructor() {
+        super();
+    }
+    create(item) {
+        return 'Postgres';
+    }
+}
+
+module.exports = PostgresStrategy;
